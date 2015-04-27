df0ce17419ce76b7d212148070467d1feca28fbd
Poornima Nookala <nookala.poornima@gmail.com>
2015-04-26 22:04:46 -0500

    added missing files

---
+++ src/client.c
@@ -0,0 +1,175 @@
+/*
+ * client.c
+ *
+ *  Created on: Apr 26, 2015
+ *      Author: pnookala
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/file.h>
+#include "mmap_common.h"
+
+// map_in is the host's input queue, etc.
+char *map_in, *map_out;
+int fin, fout;
+
+char* logfile_prefix = "mic_openmp_output";
+char* logfile_suffix = "out";
+
+void runCommand(char* command)
+{
+	sanitizeString(command);
+	int c_len = strlen(command);
+
+	char output[c_len+2];
+	memcpy(output, command, c_len * sizeof(char));
+	output[c_len] = '\n';
+	output[c_len+1] = 0;
+
+	flock(fin, LOCK_EX);
+	insert(map_in, bufferSize_in, output, c_len);
+	flock(fin, LOCK_UN);
+}
+
+void openFileAndExecute(char* path)
+{
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	stream = fopen(path, "r");
+	if (stream == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, stream)) != -1)
+	{
+		printf("> %s", line);
+		runCommand(line);
+	}
+
+	free(line);
+	fclose(stream);
+}
+
+void watchOutput(){
+
+	int i;
+	// +5 = 3 digits and 2 '.'s
+	int fname_len = strlen(logfile_prefix) + 5 + strlen(logfile_suffix);
+	char path[fname_len];
+	for(i = 0; i < 1000; i++)
+	{
+		sprintf(path, "%s.%03d.%s", logfile_prefix, i, logfile_suffix);
+		if( access(path, F_OK) == -1){
+			// file doesn't exist, use this name
+			break;
+		}
+	}
+	printf("creating output logging file: %s\n", path);
+	FILE* log_out = fopen(path, "w");
+
+	printf("starting loop\n");
+	while(1){
+
+
+		int len = strlen(map_out);
+		if(len > 0){
+			flock(fout, LOCK_EX);
+			len = strlen(map_out);
+			for (i = 0; i < len; i++) {
+				printf("%c", map_out[i]);
+				fprintf(log_out, "%c", map_out[i]);
+				map_out[i] = 0;
+			}
+			flock(fout, LOCK_UN);
+			fflush(log_out);
+		}
+
+
+		usleep(10*1000);
+	}
+
+	fclose(log_out); // useless? probably
+}
+
+int main(int argc, char *argv[])
+{
+	int filesize_in = bufferSize_in * sizeof(char);
+	int filesize_out = bufferSize_out * sizeof(char);
+
+	fin = openFile(filePathIn, filesize_in, O_RDWR);
+	map_in = mapFile(fin, filesize_in, PROT_READ | PROT_WRITE);
+
+	fout = openFile(filePathOut, filesize_out, O_RDWR);
+	map_out = mapFile(fout, filesize_out, PROT_READ | PROT_WRITE);
+
+	if(argc >= 2)
+	{
+
+		// Todo: fork(); in order to read/write to both queues
+
+		// if parent thread
+		{
+			char* arg = argv[1];
+			// If there is no file by this name
+			if( access(arg, F_OK) == -1)
+			{
+				// then we have a command
+				// Find overall command length
+				int i, length = argc-2;
+				for(i = 1; i < argc; i++)
+				{
+					length += strlen(argv[i]);
+				}
+
+				// combine argument parts into command
+				char cmd[length]; cmd[length] = 0;
+				length = 0;
+				for(i = 1;;)
+				{
+					int s = strlen(argv[i]);
+					char* currentArg = argv[i];
+					memcpy( &cmd[length], currentArg, s);
+					length += s;
+					if(++i >= argc)
+					{
+						break;
+					}
+					cmd[length++] = ' ';
+				}
+
+				printf("running command: \"%s\"\n", cmd);
+				runCommand(cmd);
+			}
+			else
+			{
+				// otherwise we have a file/script for input
+				printf("executing commands in file: %s\n", arg);
+				openFileAndExecute(arg);
+			}
+		}
+		// else if child thread
+		{
+			//watchOutput();
+		}
+
+		// wait for child
+	}
+	else
+	{
+		printf("Expected a file name or a command\n");
+		printf("Client in read-only mode\n");
+
+		watchOutput();
+	}
+}
---
+++ src/mmap_common.h
@@ -0,0 +1,184 @@
+/*
+ * mmap_common.h
+ *
+ *  Created on: Apr 26, 2015
+ *      Author: pnookala
+ */
+
+#ifndef SRC_MMAP_COMMON_H_
+#define SRC_MMAP_COMMON_H_
+
+#include <string.h>
+
+// Use gcc -DVERBOSE to enable #ifdef VERBOSE logging
+
+char* filePathIn = "/tmp/mic_openmp_shmem_in.bin";
+char* filePathOut = "/tmp/mic_openmp_shmem_out.bin";
+int bufferSize_in = 1000;
+int bufferSize_out = 2000;
+
+#define FILEMODE (0600)
+
+void createFile(char* path, int size)
+{
+#ifdef VERBOSE
+	printf("\tcreating new file\n");
+#endif
+	int fd = open(path, O_RDWR | O_CREAT | O_TRUNC, (mode_t)FILEMODE);
+	if (fd == -1) {
+		perror("Error opening file for writing");
+		exit(EXIT_FAILURE);
+	}
+
+#ifdef VERBOSE
+	printf("\tinitializing file, length %d\n", size);
+#endif
+	/* Stretch the file size to the size of the (mmapped) array of ints
+	 */
+	int result;
+	result = lseek(fd, size-1, SEEK_SET);
+	if (result == -1) {
+		close(fd);
+		perror("Error calling lseek() to 'stretch' the file");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Something needs to be written at the end of the file to
+	 * have the file actually have the new size.
+	 * Just writing an empty string at the current file position will do.
+	 *
+	 * Note:
+	 *  - The current position in the file is at the end of the stretched
+	 *	file due to the call to lseek().
+	 *  - An empty string is actually a single '\0' character, so a zero-byte
+	 *	will be written at the last byte of the file.
+	 */
+	result = write(fd, "", 1);
+	if (result != 1) {
+		close(fd);
+		perror("Error writing last byte of the file");
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+#ifdef VERBOSE
+	printf("\tfile created\n");
+#endif
+}
+
+// Opens the file and creates it if it doesn't exist.
+// File will be created with the length specified, but
+// if it exists is not guaranteed to have the proper length
+int openFile(char* path, int size, int mode)
+{
+#ifdef VERBOSE
+	printf("Opening file: %s\n", path);
+#endif
+	if( access(path, F_OK) == -1){
+		createFile(path, size);
+	}
+
+#ifdef VERBOSE
+	printf("\topening file with mode: %d\n", mode);
+#endif
+	int fd = open(path, mode, (mode_t)FILEMODE);
+	if (fd == -1) {
+		perror("Error opening file for mmap-ing");
+		exit(EXIT_FAILURE);
+	}
+	return fd;
+}
+
+// mode will be "PROT_READ | PROT_WRITE" in most cases
+char* mapFile(int fd, int fileSize, int mode)
+{
+#ifdef VERBOSE
+	printf("mmap-ing file\n");
+#endif
+	char* map = mmap(0, fileSize, mode, MAP_SHARED, fd, 0);
+	if (map == MAP_FAILED) {
+		close(fd);
+		perror("Error mmapping the file");
+		exit(EXIT_FAILURE);
+	}
+	return map;
+}
+
+//int openFile(char* path, int size)
+//{
+//	return openFile(path, size, O_RDWR);
+//}
+
+
+int canPut(char* target, int capacity, int put_size)
+{
+	int filled = strlen(target);
+	return (capacity > filled + put_size);
+}
+//bool insert(char* message)
+
+void sanitizeString(char* str)
+{
+	int i;
+	for(i = 0; str[i] > 0; i++){
+		if((int)str[i] < 32){
+			str[i] = ' ';
+		}
+	}
+
+	for(i = strlen(str); i > 0; i--)
+	{
+		if((int)str[i] > 32)
+			break;
+		str[i] = 0;
+	}
+}
+
+// puts the specified <command> in <queue>
+// Warning: may cause deadlocks when queue_capacity is exceeded and
+// method is forced to wait for queue to empty. Untested. In future,
+// release the file lock when in the while() loop
+void insert(char* queue, int queue_capacity, char* command, int dummy)
+{
+	int c_len = strlen(command);
+
+#ifdef VERBOSE
+	int i;
+	printf("insert\n");
+	printf("\tmsg: \"%s\"\n", command);
+	printf("\t");
+	for(i=0; i<c_len; i++)
+	{
+		printf("%02x", command[i]);
+		if( i % 2 == 1)
+			printf(" ");
+	}
+	printf("\n");
+	printf("\tqueue len/cap: %d/%d\n", strlen(queue), queue_capacity);
+	printf("\twrite length:  %d\n", c_len);
+	printf("\tcan write? %d\n", canPut(queue, queue_capacity, c_len));
+#endif
+
+	while(!canPut(queue, queue_capacity, c_len))
+	{
+		usleep(10*1000);
+#ifdef VERBOSE
+		printf("\tcan write? %d\n", canPut(queue, queue_capacity, c_len));
+#endif
+	}
+
+	// must be acquired after canPut allows us to write
+	int queue_len = strlen(queue);
+
+#ifdef VERBOSE
+	printf("\t[%d, %d)\n", queue_len, queue_len + c_len);
+#endif
+
+	// Puts this command at the end of the queue (at queue_len)
+	memcpy( &queue[queue_len], command, c_len * sizeof( char ) );
+	// Ensure it's null terminated (even though it should be anyways)
+	queue[queue_len + c_len] = 0;
+
+}
+
+
+#endif /* SRC_MMAP_COMMON_H_ */
